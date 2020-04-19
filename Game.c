f96a60248ee934216b5bbeb3218848d1b49235c6
Kaboinglefop <63935881+Kaboinglefop@users.noreply.github.com>
2020-04-18 20:19:00 -0400

    Add files via upload

---
+++ Game.c
@@ -0,0 +1,142 @@
+#include "msp.h"
+#include "BSP.h"
+#include "G8RTOS_Lab3/G8RTOS.h"
+#include "G8RTOS_Lab3/G8RTOS_IPC.h"
+#include "led/led.h"
+#include "Game.h"
+#include <stdbool.h>
+#include "G8RTOS_Lab3/G8RTOS_Structures.h"
+
+
+#define LCD_WHITE          0xFFFF
+#define LCD_BLACK          0x0000
+#define LCD_BLUE           0x0197
+#define LCD_RED            0xF800
+#define LCD_MAGENTA        0xF81F
+#define LCD_GREEN          0x07E0
+#define LCD_CYAN           0x7FFF
+#define LCD_YELLOW         0xFFE0
+#define LCD_GRAY           0x2104
+#define LCD_PURPLE         0xF11F
+#define LCD_ORANGE         0xFD20
+#define LCD_PINK           0xfdba
+#define LCD_OLIVE          0xdfe4
+
+
+/*********************************************** Client Threads *********************************************************************/
+/*
+ * Thread for client to join game
+ */
+void JoinGame();
+
+/*
+ * Thread that receives game state packets from host
+ */
+void ReceiveDataFromHost();
+
+/*
+ * Thread that sends UDP packets to host
+ */
+void SendDataToHost();
+
+/*
+ * Thread to read client's joystick
+ */
+void ReadJoystickClient();
+
+/*
+ * End of game for the client
+ */
+void EndOfGameClient();
+
+/*********************************************** Client Threads *********************************************************************/
+
+
+/*********************************************** Host Threads *********************************************************************/
+/*
+ * Thread for the host to create a game
+ */
+void CreateGame();
+
+/*
+ * Thread that sends game state to client
+ */
+void SendDataToClient();
+
+/*
+ * Thread that receives UDP packets from client
+ */
+void ReceiveDataFromClient();
+
+/*
+ * Generate Ball thread
+ */
+void GenerateBall();
+
+/*
+ * Thread to read host's joystick
+ */
+void ReadJoystickHost();
+
+/*
+ * Thread to move a single ball
+ */
+void MoveBall();
+
+/*
+ * End of game for the host
+ */
+void EndOfGameHost();
+
+/*********************************************** Host Threads *********************************************************************/
+
+
+/*********************************************** Common Threads *********************************************************************/
+/*
+ * Idle thread
+ */
+void IdleThread()
+{
+    while(1);
+}
+
+/*
+ * Thread to draw all the objects in the game
+ */
+void DrawObjects();
+
+/*
+ * Thread to update LEDs based on score
+ */
+void MoveLEDs();
+
+/*********************************************** Common Threads *********************************************************************/
+
+
+/*********************************************** Public Functions *********************************************************************/
+/*
+ * Returns either Host or Client depending on button press
+ */
+playerType GetPlayerRole();
+
+/*
+ * Draw players given center X center coordinate
+ */
+void DrawPlayer(GeneralPlayerInfo_t * player);
+
+/*
+ * Updates player's paddle based on current and new center
+ */
+void UpdatePlayerOnScreen(PrevPlayer_t * prevPlayerIn, GeneralPlayerInfo_t * outPlayer);
+
+/*
+ * Function updates ball position on screen
+ */
+void UpdateBallOnScreen(PrevBall_t * previousBall, Ball_t * currentBall, uint16_t outColor);
+
+/*
+ * Initializes and prints initial game state
+ */
+void InitBoardState();
+
+/*********************************************** Public Functions *********************************************************************/
